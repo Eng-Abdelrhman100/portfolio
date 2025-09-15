b9f01fe2073bc68744c862693fa58615808f6e31
Eng-Abdelrhman100 <engabdelrhmanyasser100@gmail.com>
2025-09-15 11:31:05 +0300

    My Portfolio

---
+++ data/index.ts
@@ -0,0 +1,120 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Grand Project",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Nike Clone Store",
+    des: "Experience a sleek, modern shopping  all inspired by Nike's iconic designs.",
+    img: "/nike-clone.png",
+    iconLists: ["/re.svg", "/tail.svg", "/fm.svg"],
+    link: "https://nike-shop-lyart.vercel.app",
+  },
+  {
+    id: 2,
+    title: "Zentry - Gaming Platform Clone",
+    des: "Immersive gaming platform at your fingertips, inspired by Zentry.",
+    img: "/zentry-clone.png",
+    iconLists: ["/re.svg", "/tail.svg", "/fm.svg"],
+    link: "https://zentry-pearl.vercel.app",
+  },
+];
+
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Freelance App Dev Project",
+    desc: "Collaborated with a team to develop a website using React.js and Node.js.",
+    className: "md:col-span-2", 
+    thumbnail: "/exp3.svg",
+  },
+  
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Eng-Abdelrhman100",
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/abdelrhman-yasser-ali-ibrahim-734911255",
+  },
+];
